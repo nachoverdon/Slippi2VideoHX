4938f3e899eeafcb8acf9374c6a547ea527dda57
Nacho Verdón <nverblaz@gmail.com>
2019-04-16 21:11:39 +0200

    button listeners, switches -> checkboxes

--- src/ui/UI.hx
+++ src/ui/UI.hx
@@ -1,14 +1,16 @@
 package ui;
 
-import haxe.ui.containers.VBox;
-import haxe.ui.components.Switch;
-import haxe.ui.components.Button;
-import haxe.ui.components.TextField;
+import haxe.ui.containers.HBox;
 import haxe.ui.Toolkit;
 import lime.ui.FileDialog;
 import haxe.ui.core.Screen;
 import lime.ui.FileDialogType;
 import haxe.ui.containers.Grid;
+import haxe.ui.containers.VBox;
+import haxe.ui.events.MouseEvent;
+import haxe.ui.components.Button;
+import haxe.ui.components.CheckBox;
+import haxe.ui.components.TextField;
 import haxe.ui.macros.ComponentMacros;
 
 class UI extends openfl.display.Sprite
@@ -16,15 +18,17 @@ class UI extends openfl.display.Sprite
 	static var leftColumn: VBox;
 	static var rightColumn: VBox;
 
+	static var components: HBox;
+	static var buttons: HBox;
+
 	static var paths: Grid;
 	static var others: Grid;
-	static var switches: Grid;
 
-	static var dolphinPath: TextField;
-	static var replaysPath: TextField;
-	static var obsPath: TextField;
-	static var meleePath: TextField;
-	static var videosPath: TextField;
+	static var dolphinTextField: TextField;
+	static var replaysTextField: TextField;
+	static var obsTextField: TextField;
+	static var meleeTextField: TextField;
+	static var videosTextField: TextField;
 
 	static var dolphinButton: Button;
 	static var replaysButton: Button;
@@ -32,32 +36,43 @@ class UI extends openfl.display.Sprite
 	static var meleeButton: Button;
 	static var videosButton: Button;
 
-	static var port: TextField;
-	static var password: TextField;
-	static var profile: TextField;
-	static var scene: TextField;
+	static var portTextField: TextField;
+	static var passwordTextField: TextField;
+	static var profileTextField: TextField;
+	static var sceneTextField: TextField;
 
-	static var recurse: Switch;
-	static var rename: Switch;
-	static var restructure: Switch;
-	static var kill: Switch;
+	static var recursive: CheckBox;
+	static var rename: CheckBox;
+	static var restructure: CheckBox;
+	static var kill: CheckBox;
 
-	public static function initComponents() {
+	static var saveButton: Button;
+
+	public function new() {
+		super();
+
+		initComponents();
+		setStyles();
+	}
+
+	static function initComponents(): Void {
 		Toolkit.init();
 		var ui = ComponentMacros.buildComponent('assets/ui/ui.xml');
 		Screen.instance.addComponent(ui);
 		leftColumn = ui.findComponent('leftColumn', VBox);
 		rightColumn = ui.findComponent('rightColumn', VBox);
 
+		components = ui.findComponent('components', HBox);
+		buttons = ui.findComponent('buttons', HBox);
+
 		paths = ui.findComponent('paths', Grid);
 		others = ui.findComponent('others', Grid);
-		switches = ui.findComponent('switches', Grid);
 
-		dolphinPath = ui.findComponent('dolphinPath', TextField);
-		replaysPath = ui.findComponent('replaysPath', TextField);
-		obsPath = ui.findComponent('obsPath', TextField);
-		meleePath = ui.findComponent('meleePath', TextField);
-		videosPath = ui.findComponent('videosPath', TextField);
+		dolphinTextField = ui.findComponent('dolphinTextField', TextField);
+		replaysTextField = ui.findComponent('replaysTextField', TextField);
+		obsTextField = ui.findComponent('obsTextField', TextField);
+		meleeTextField = ui.findComponent('meleeTextField', TextField);
+		videosTextField = ui.findComponent('videosTextField', TextField);
 
 		dolphinButton = ui.findComponent('dolphinButton', Button);
 		replaysButton = ui.findComponent('replaysButton', Button);
@@ -65,36 +80,75 @@ class UI extends openfl.display.Sprite
 		meleeButton = ui.findComponent('meleeButton', Button);
 		videosButton = ui.findComponent('videosButton', Button);
 
-		port = ui.findComponent('port', TextField);
-		password = ui.findComponent('password', TextField);
-		profile = ui.findComponent('profile', TextField);
-		scene = ui.findComponent('scene', TextField);
+		portTextField = ui.findComponent('port', TextField);
+		passwordTextField = ui.findComponent('password', TextField);
+		profileTextField = ui.findComponent('profile', TextField);
+		sceneTextField = ui.findComponent('scene', TextField);
 
-		recurse = ui.findComponent('recurse', Switch);
-		rename = ui.findComponent('rename', Switch);
-		restructure = ui.findComponent('restructure', Switch);
-		kill = ui.findComponent('kill', Switch);
+		recursive = ui.findComponent('recursive', CheckBox);
+		rename = ui.findComponent('rename', CheckBox);
+		restructure = ui.findComponent('restructure', CheckBox);
+		kill = ui.findComponent('kill', CheckBox);
+
+		saveButton = ui.findComponent('saveButton', Button);
+
+		setEventListeners();
 	}
 
-	public function new()
-	{
-		super();
-		initComponents();
-		leftColumn.marginLeft = 10;
-		leftColumn.marginTop = 10;
-		leftColumn.marginBottom = 10;
-		leftColumn.marginRight = 10;
-		rightColumn.marginLeft = 10;
-		rightColumn.marginTop = 10;
-		rightColumn.marginBottom = 10;
-		rightColumn.marginRight = 10;
+	static function setEventListeners(): Void {
 
-		var fd = new FileDialog();
-		fd.browse(FileDialogType.OPEN, 'exe', null, 'Dolphin.exe');
+		inline function onSelect(fd: FileDialog, tf: TextField): Void {
+			fd.onSelect.add(function(s: String) {tf.text = s;});
+		}
 
-		fd.onSelect.add(function(path: String) {
-			trace(path);
-		});
+		inline function onClick(fdt: FileDialogType, name: String, tf: TextField, ?filter: String): MouseEvent -> Void {
+			var fd = new FileDialog();
+
+			return function(evt: MouseEvent) {
+				onSelect(fd, tf);
+				fd.browse(fdt, filter, null, name);
+			}
+		}
+
+		dolphinButton.onClick = onClick(
+			FileDialogType.OPEN, 'Dolphin.exe', dolphinTextField, 'exe'
+		);
+
+		replaysButton.onClick = onClick(
+			FileDialogType.OPEN_DIRECTORY, 'Replays folder', replaysTextField
+		);
+
+		obsButton.onClick = onClick(
+			FileDialogType.OPEN, 'OBS32.exe / OBS64.exe', obsTextField, 'exe'
+		);
+
+		meleeButton.onClick = onClick(
+			FileDialogType.OPEN, 'Melee NTSC 1.02 .iso', meleeTextField, 'iso'
+		);
+
+		videosButton.onClick = onClick(
+			FileDialogType.OPEN_DIRECTORY, 'Videos folder', videosTextField
+		);
+
+		saveButton.onClick = saveConfig;
+	}
+
+	static function setStyles(): Void {
+		for (box in [leftColumn, rightColumn, buttons]) {
+			box.marginLeft = 10;
+			box.marginTop = 10;
+			box.marginBottom = 10;
+			box.marginRight = 10;
+		}
+
+	}
+
+	static function loadConfig(): Void {
+
+	}
+
+	static function saveConfig(evt: MouseEvent): Void {
+
 	}
 
 }
